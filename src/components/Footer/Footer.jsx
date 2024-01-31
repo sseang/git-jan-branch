46bd99b9d7b257c0eab8a2de5318807d67d7ca5a
sseang <142060734+sseang@users.noreply.github.com>
2024-01-30 21:38:04 -0600

    added text

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,46 +1,47 @@
 import react, { useEffect } from 'react';
-import {
-    resetList,
-    updateListPurchased
-} from '../../itemAPI/item.api';
 
-function Footer({itemData, shoppingListRefreshCall}) {
-    const handleClickClear = () => {
-        console.log('reset shopping list');
-        resetList()
-            .then((response) => {
-                shoppingListRefreshCall();
-            })
-            .catch((err) => {
-                console.error('ERROR', err);
-            })
-    };
+import { resetList, updateListPurchased } from '../../itemAPI/item.api';
 
-    const handleClickResetPurchased = () => {
-        console.log('reset purchased items');
-        updateListPurchased()
-        .then((response) => {
-            shoppingListRefreshCall();
-        })
-        .catch((err) => {
-            console.error('ERROR', err);
-        })
-    };
-
-    useEffect(() => {
-        //body
-        console.log('Welcome shopper');
-        //api call
+function Footer({ itemData, shoppingListRefreshCall }) {
+  const handleClickClear = () => {
+    console.log('reset shopping list');
+    resetList()
+      .then((response) => {
         shoppingListRefreshCall();
-    }, []);
-    
+      })
+      .catch((err) => {
+        console.error('ERROR', err);
+      });
+  };
 
-    return (
-        <>
-        <button onClick={(event) => handleClickClear(itemData)}>Clear List</button>
-        <button onClick={(event) => handleClickResetPurchased()}>Reset Purchased</button>
-        </>
-    )
-};
+  const handleClickResetPurchased = () => {
+    console.log('reset purchased items');
+    updateListPurchased()
+      .then((response) => {
+        shoppingListRefreshCall();
+      })
+      .catch((err) => {
+        console.error('ERROR', err);
+      });
+  };
+
+  useEffect(() => {
+    //body
+    console.log('Welcome shopper');
+    //api call
+    shoppingListRefreshCall();
+  }, []);
+
+  return (
+    <div>
+      <button onClick={(event) => handleClickClear(itemData)}>
+        Clear List!
+      </button>
+      <button onClick={(event) => handleClickResetPurchased()}>
+        Reset Purchased!
+      </button>
+    </div>
+  );
+}
 
 export default Footer;
